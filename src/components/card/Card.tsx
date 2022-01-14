32acd53bf8640f9187334f4e4588a1891a777c3c
Aleaskn <alessandro.cristalli@hotmail.it>
2022-01-14 16:50:07 +0100

    fix-overlay-img

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -11,16 +11,28 @@ import ImgAvatar from '../../assets/img/image-avatar.png'
 
 const Card : React.FC<{}> = (props) => {
     return (
-       // <section className='card__container'>
-        <div className='card'>
-            <div className='card__img'>
-                <img src={ImgEquilibrium} alt='equilibrium'/>
-            </div>
+
+       <section className='card__container'>
+
+            <div className='card'>
+
+                <div className='card__img'>
+
+                    <img src={ImgEquilibrium} alt='equilibrium' />
+
+                    <div className='img__icon-view'>
+
+                        <IconView />
+
+                     </div>
+
+                </div> {/* end card__img */}
+
             <div className='card__info'>
                 
                 <div className='card__title'>
                     
-                    <h2 className='card__title'>Equilibrium #3429</h2>
+                    <h2 className='card__title'>equilibrium #3429</h2>
 
                 </div>
 
@@ -29,38 +41,46 @@ const Card : React.FC<{}> = (props) => {
                 <div className='cripto-info'>
 
                     <div className='cripto-info__value'>
-                        <div className='cripto__Eth'>
-                            <IconEth></IconEth>
-                        </div>
-                        <h3>0.041ETH</h3>
+
+                            <IconEth className='cripto__eth' />
+
+                        <h3>0.041eth</h3>
+
                     </div>
 
                     <div className='cripto-info__data'>
-                        <div className='cripto__clock'>
-                            <IconData></IconData>
-                        </div>
+
+                            <IconData className='cripto__data'/>
+
                         <h3>3 days left</h3>
+
                     </div>
 
                 </div>
             
             </div>
-                        <div className='card__line'>
 
-                        </div>
+            <div className='card__line'></div> {/* end card__line */}
 
-                    <div className='card__creator'>
-                            <div className='card__creator__img'>
-                                 <img src={ImgAvatar}/>
-                            </div>
+                <div className='card__creator'>
 
-                            <div className='card__creator__name'>
-                               <h3>Creation of <a href="#">Jules Wyvern</a></h3> 
-                                
-                            </div>
+                    <div className='card__creator__img'>
+
+                        <img src={ImgAvatar}/>
+
                     </div>
-        </div>
-       // </section>
+
+                    <div className='card__creator__name'>
+
+                        <h3>creation of <a href="#">Jules Wyvern</a></h3> 
+                                
+                    </div>
+
+                </div>
+
+            </div>
+
+     </section>
     )
 }
 
